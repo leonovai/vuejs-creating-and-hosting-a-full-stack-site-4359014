82f12623b8c23caf43b265049b28ef65d49967fb
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-14 10:01:11 +0100

    Drafted product details page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Product Detail Page</h1>
+  <div class="img-wrap">
+    <img :src="product.imageName" />
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add to Cart</button>
+  </div>
 </template>
 
 <script>
@@ -9,7 +16,7 @@ export default {
   name: 'ProductDetailPage',
   data() {
     return {
-      products,
+      product: products.find(product => product.id === this.$route.params.productId)
     }
   }
 }
