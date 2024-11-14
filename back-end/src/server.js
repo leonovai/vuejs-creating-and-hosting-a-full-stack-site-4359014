ce0bfc0af745ff420fd44dad1ca14ab3b0f54ee8
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-14 15:34:10 +0100

    Added further endpoints

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,23 @@
 import express from "express";
+import { cartItems, products } from "./temp-data";
 
 const app = express();
 
 app.get("/hello", (_, res) => {
-  res.send("Hello!");
+  res.send("Hello111!");
+});
+
+app.get("/products", (_, res) => {
+  res.json(products);
+});
+
+app.get("/cart", (_, res) => {
+  res.json(cartItems);
+});
+
+app.get("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
+  res.json(products.find((product) => product.id === productId));
 });
 
 app.listen(8000, () => {
