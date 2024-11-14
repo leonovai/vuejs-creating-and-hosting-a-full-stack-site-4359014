ab2d747d5ce3dc2f00b80f85ff75a161e0f1bf1a
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-14 10:28:02 +0100

    Drafted cart page

--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Shopping Cart Page</h1>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <div v-for="product in cartItems" :key="product.id" class="product-container">
+      <img :src="product.imageName" class="product-image" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove</button>
+    </div>
+    <button class="checkout-button">Checkout</button>
+  </div>
+  <div v-else>
+    <p>Your cart is empty</p>
+  </div>
 </template>
 
 <script>
+import { cartItems } from '../temp-data'
+
 export default {
   name: 'ShoppingCartPage',
+  data() {
+    return {
+      cartItems,
+    }
+  }
 }
 </script>
--- front-end/src/temp-data.js
+++ front-end/src/temp-data.js
@@ -7,6 +7,8 @@ import delicateBottle from './assets/delicate-bottle.webp';
 import mediumBottle from './assets/medium-bottle.webp';
 import boldBottle from './assets/bold-bottle.webp';
 
+export const cartItems = []
+
 export const products = [{
     id: '123',
     name: 'Basil',
