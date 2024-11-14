8bdab5c84888926e5edbce0523eb76abe2254967
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-14 15:56:30 +0100

    Added post method to add product to cart

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { cartItems, products } from "./temp-data";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_, res) => {
   res.send("Hello!");
@@ -18,6 +19,13 @@ app.get("/cart", (_, res) => {
 app.get("/product/:productId", (req, res) => {
   const productId = req.params.productId;
   res.json(products.find((product) => product.id === productId));
+});
+
+app.post("/cart", (req, res) => {
+  const productId = String(req.body.id);
+  const product = products.find((product) => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
 });
 
 app.listen(8000, () => {
