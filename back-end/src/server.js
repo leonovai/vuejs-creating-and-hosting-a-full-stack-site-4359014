716d4a2002ec1c17bd7440e2876523f889d440b5
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-18 15:28:50 +0100

    Retired redundant mongo client connection

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,72 +5,67 @@ import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data'
 let cartItems = cartItemsRaw;
 let products = productsRaw;
 
-const dbUrl = 'mongodb+srv://newuser:IuiQpO0uboSoE4Pe@mycluster.pezg5.mongodb.net/'
-const client = new MongoClient(dbUrl, {
-  tls: true,
-  tlsCAFile: '/etc/ssl/certs/CatoNetworksTrustedRootCA.pem'
-})
+async function start() {
 
-const app = express();
-app.use(express.json());
+  const dbUrl = 'mongodb+srv://newuser:IuiQpO0uboSoE4Pe@mycluster.pezg5.mongodb.net/'
+  const client = new MongoClient(dbUrl, {
+    tls: true,
+    tlsCAFile: '/etc/ssl/certs/CatoNetworksTrustedRootCA.pem'
+  })
 
-app.get('/products', async (req, res) => {
   await client.connect();
   const db = client.db('vue-db');
-  products = await db.collection('products').find({}).toArray();
-  res.json(products);
-});
 
-async function populateCartIds(ids) {
-  await client.connect();
-  const db = client.db('vue-db');
-  return Promise.all(
-    ids.map(id => db.collection('products').findOne({ id }))
-  );
-  // return ids.map(id => products.find(product => product.id === id));
-}
+  const app = express();
+  app.use(express.json());
 
-app.get('/users/:userId/cart', async (req, res) => {
-  const userId = req.params.userId;
-  await client.connect();
-  const userDB = client.db('vue-db');
-  const user = await userDB.collection('users').findOne({ id: userId });
-  const populatedCart = await populateCartIds(user.cartItems);
-  res.json(populatedCart);
-});
+  app.get('/products', async (req, res) => {
+    products = await db.collection('products').find({}).toArray();
+    res.json(products);
+  });
 
-app.get('/products/:productId', (req, res) => {
-  const productId = req.params.productId;
-  const product = products.find(product => product.id === productId);
-  res.json(product);
-});
-
-app.post('/cart', (req, res) => {
-  const productId = req.body.id;
-  cartItems.push(productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.delete('/cart/:productId', (req, res) => {
-  const productId = req.params.productId;
-  cartItems = cartItems.filter(id => id !== productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.delete("/cart/:productId", (req, res) => {
-  const productId = req.params.productId;
-  const index = cartItems.findIndex(product => product.id === productId);
-
-  if (index !== -1) {
-    cartItems.splice(index, 1);
+  async function populateCartIds(ids) {
+    return Promise.all(
+      ids.map(id => db.collection('products').findOne({ id }))
+    );
+    // return ids.map(id => products.find(product => product.id === id));
   }
 
-  console.log(cartItems);
-  res.json(cartItems);
-});
+  app.get('/users/:userId/cart', async (req, res) => {
+    const userId = req.params.userId;
+    const user = await db.collection('users').findOne({ id: userId });
+    const populatedCart = await populateCartIds(user.cartItems);
+    res.json(populatedCart);
+  });
 
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000')
-});
+  app.get('/products/:productId', async (req, res) => {
+    const productId = req.params.productId;
+    const product = await db.collection('products').findOne({ id: productId });
+    res.json(product);
+  });
+
+  app.post('/cart', (req, res) => {
+    const productId = req.body.id;
+    cartItems.push(productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  app.delete("/cart/:productId", (req, res) => {
+    const productId = req.params.productId;
+    const index = cartItems.findIndex(product => product.id === productId);
+
+    if (index !== -1) {
+      cartItems.splice(index, 1);
+    }
+
+    console.log(cartItems);
+    res.json(cartItems);
+  });
+
+  app.listen(8000, () => {
+    console.log('Server is listening on port 8000')
+  });
+}
+
+start()
