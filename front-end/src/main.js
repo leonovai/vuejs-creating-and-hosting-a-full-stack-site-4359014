94958c868d0b70c60ef26948d094058e7f47ae2b
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-20 15:01:27 +0100

    WIP: initial set up for firebase

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,6 +7,24 @@ import ProductDetailPage from "./pages/ProductDetailPage.vue";
 import ProductsPage from "./pages/ProductsPage.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbGyIIUGw-03m94fk1ribXH_Cd7pY2HcA",
+  authDomain: "myproject-c7e1a.firebaseapp.com",
+  projectId: "myproject-c7e1a",
+  storageBucket: "myproject-c7e1a.firebasestorage.app",
+  messagingSenderId: "429022019422",
+  appId: "1:429022019422:web:6713833fcb3151210b19cf",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createApp(App)
   .use(
     VueRouter.createRouter({
