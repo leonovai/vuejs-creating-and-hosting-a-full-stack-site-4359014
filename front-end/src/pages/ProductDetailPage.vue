a4a654d962a5a2d8b6659ca331017da1e48666f7
Artem Leonov <artem.leonov@carlsberg.com>
2024-11-20 15:39:06 +0100

    WIP: including firebase authentication

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -8,6 +8,7 @@
       <h3 class="price">{{ product.price }}</h3>
       <button class="add-to-cart" @click="addToCart" v-if="!alreadyInCart">Add to Cart</button>
       <button v-else class="grey-button">Already in cart</button>
+      <button v-if="!isLoggedIn" class="sign-in" @click="signIn">Sign in to add to cart</button>
     </div>
   </div>
   <div v-else>
@@ -16,6 +17,7 @@
 </template>
 
 <script>
+import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
 import NotFoundPage from './NotFoundPage.vue'
 import axios from 'axios';
 
@@ -25,6 +27,7 @@ export default {
     return {
       product: {},
       alreadyInCart: false,
+      isLoggedIn: false,
     }
   },
   methods: {
@@ -38,18 +41,37 @@ export default {
       );
       alert("Added to cart!");
       this.alreadyInCart = true;
+    },
+    async signIn() {
+      const email = prompt("Enter your email");
+      const auth = getAuth();
+      const actionCodeSettings = {
+        url: `http://localhost:8080/products/${this.$route.params.productId}`,
+        handleCodeInApp: true,
+      };
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      alert("Check your email");
+      window.localStorage.setItem("emailForSignIn", email);
     }
   },
   components: {
     NotFoundPage,
   },
   async created() {
-    const response = await axios.get(`/api/products/${this.$route.params.productId}`)
+    const auth = getAuth();
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      const email = window.localStorage.getItem("emailForSignIn");
+      await signInWithEmailLink(auth, email, window.location.href);
+      alert("Signed in!");
+      window.localStorage.removeItem("emailForSignIn");
+    }
+
+    const response = await axios.get(`/api/products/${this.$route.params.productId}`);
     this.product = response.data;
 
-    const cartResponse = await axios.get('/api/users/12345/cart')
+    const cartResponse = await axios.get('/api/users/12345/cart');
     const cartItemsIds = cartResponse.data.map(item => item.id);
-    this.alreadyInCart = cartItemsIds.includes(this.product.id)
+    this.alreadyInCart = cartItemsIds.includes(this.product.id);
   }
 }
 </script>
